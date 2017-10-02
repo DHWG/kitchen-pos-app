25a1de3f7fda82e76c3d87df5286b8fcc31d0044
Jeffrey Jedele <jeffrey.jedele@gmail.com>
2017-10-02 18:34:34 +0200

    Fullscreen, adding a couple of buttons

--- app/src/main/java/dhwg/com/wgpos/MainActivity.kt
+++ app/src/main/java/dhwg/com/wgpos/MainActivity.kt
@@ -2,11 +2,27 @@ package dhwg.com.wgpos
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            // enter immersive fullscreen mode to hide all navigation bards
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
     }
 }
