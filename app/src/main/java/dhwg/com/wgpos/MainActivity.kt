5087b611ce5b79d852b6ef210e2f5389811b3e2d
Jeffrey Jedele <jeffrey.jedele@gmail.com>
2017-10-03 20:22:19 +0200

    Fullscreen on create.

--- app/src/main/java/dhwg/com/wgpos/MainActivity.kt
+++ app/src/main/java/dhwg/com/wgpos/MainActivity.kt
@@ -17,25 +17,20 @@ class MainActivity : Activity() {
 
         setContentView(R.layout.activity_main)
 
+        // enter immersive fullscreen mode to hide all navigation bards
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE)
+
         val glay = findViewById(R.id.griddy) as GridLayout
         DHWGManagementAPI.getInhabitants { inhabitants ->
             for (inhabitant in inhabitants) {
                 val button = createButton(inhabitant)
                 glay.addView(button)
             }
-        }
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
-            // enter immersive fullscreen mode to hide all navigation bards
-            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
         }
     }
 
--- app/src/main/java/dhwg/com/wgpos/SummaryActivity.kt
+++ app/src/main/java/dhwg/com/wgpos/SummaryActivity.kt
@@ -24,23 +24,21 @@ class SummaryActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
 
+        // enter immersive fullscreen mode to hide all navigation bards
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE)
+
         val button = findViewById(R.id.back_button) as Button
         button.setOnClickListener(ButtonListener())
-
-
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) {
-            // enter immersive fullscreen mode to hide all navigation bards
-            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
-
             val amountText = findViewById(R.id.amount_text_view) as TextView
             val buyerId = intent.getIntExtra("buyerId", -1)
             DHWGManagementAPI.getInhabitant(buyerId, { inhabitant ->
